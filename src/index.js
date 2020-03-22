f925484e1fe87bd3be742c38727cfe4af7e63985
Aliaksandr Huzau <huzau.aliaksandr@gmail.com>
2020-03-22 16:26:19 +0300

    complete reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let absN = Math.abs( n );
+    if(absN !== NaN) {
+        return absN.toString().split('').reverse().join('');
+    } else {
+        return 0;
+    }
 }
